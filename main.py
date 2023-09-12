3cdd19e5c93eaf8cc7c2b4d06c11096fb147dd3b
Joan Contreras <rvanfx@gmail.com>
2023-09-12 19:35:30 -0400

    Added select file by file explorer.

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import requests
 import uuid
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
 
 def Main(dictionary):
     uid = uuid.uuid4()
     passwords = []
+    dictionary_selected = False
 
-    proxies = "proxies.txt"
+    # proxies = "proxies.txt"
 
-    with open(proxies, 'r') as f:
-        lproxies = f.read().splitlines()
+    # with open(proxies, 'r') as f:
+    #     lproxies = f.read().splitlines()
 
-    with open(dictionary, "r") as f:
-        for line in f:
-            passwords.append(line.strip())
+    try:
+        if(dictionary):
+            with open(dictionary, "r") as f:
+                for line in f:
+                    passwords.append(line.strip())
+
+    except(Exception):
+        print("No file found in that path, make sure you put the full path.")
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36"
     }
 
-    for proxy in lproxies:
-        proxy_dict = {
-            'http': f'http://{proxy}',
-            'https': f'https://{proxy}'
-        }
+    # for proxy in lproxies:
+    #     proxy_dict = {
+    #         'http': f'http://{proxy}',
+    #         'https': f'https://{proxy}'
+    #     }
 
     
     user = input("Enter the user to recover: ")
@@ -42,6 +53,7 @@ def Main(dictionary):
             break
 
 if __name__ == "__main__":
-    dictionary = input("Enter dictionary: ")
+    print("Enter dictionary: ")
+    dictionary = filedialog.askopenfilename()
     Main(dictionary=dictionary)
     input("Press any key to continue...")
